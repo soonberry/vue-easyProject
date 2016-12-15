bf25159ecd313f29ba664432745862569f655bb7
soonberry <shuwenli89@126.com>
2016-12-15 23:39:46 +0800

    [Shuwen] Base Code.

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import Com from './Com'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#hello',
+  components: { Com }
+})
